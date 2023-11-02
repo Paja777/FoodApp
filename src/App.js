662ac1c64b96b99c3804c0d405b0e421fd690e0a
Paja777 <pavle.rvovic95@gmail.com>
2023-11-02 18:44:19 +0100

    Project repo

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { Fragment, useState } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from './store/CartProvider';
+
+function App() {
+  const [showCart, setShowCart] = useState(false);
+  
+  const showCartHandler = () =>{
+    setShowCart(true);
+  };
+  const closeCartHandler = () =>{
+    setShowCart(false);
+  };
+
+  return (
+    <CartProvider>
+      {showCart && <Cart onClose={closeCartHandler}/>}
+      <Header onCartIcon={showCartHandler}/>
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
